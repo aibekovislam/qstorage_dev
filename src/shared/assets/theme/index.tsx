48eae1a6ed1b89c1639fb22aaba488b1852d27f2
aibekovislam <islamxdaamduu@gmail.com>
2024-12-27 13:32:59 +0600

    [FEAT]: auth page

--- src/shared/assets/theme/index.tsx
+++ src/shared/assets/theme/index.tsx
@@ -1,8 +1,21 @@
 import { ThemeConfig } from 'antd'
 
 const theme: ThemeConfig = {
+  components: {
+    Button: {
+      borderRadius: 3,
+    },
+    Input: {
+      borderRadius: 3,
+      paddingBlock: 10,
+      paddingInline: 15,
+      fontFamily: 'var(--font-roboto)',
+    },
+  },
   token: {
-    fontFamily: 'var(--font-golos)',
+    fontFamily: 'var(--font-roboto)',
+    colorPrimary: '#FA541C',
+    colorLink: '#FA541C',
   },
 } as const
 
---
+++ src/shared/ui/textfield/textfield.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+
+import { Input, InputProps } from 'antd'
+import { Rule } from 'antd/es/form'
+import FormItem from 'antd/es/form/FormItem'
+
+import cls from './textfield.module.css'
+
+interface Props extends InputProps {
+    label?: string
+    initialValue?: string
+    text?: string
+    rules?: Rule[]
+    maxLength?: number
+}
+
+export const TextField: React.FC<Props> = (props) => {
+  return (
+    <FormItem
+      className={`${cls.textField} ${props.disabled && cls.textField_disabled}`}
+      label={props.label}
+      style={props.style}
+    >
+      {
+        props.text ? (
+          <p className={cls.textField__description}>
+            {props.text}
+          </p>
+        ) : null
+      }
+
+      <FormItem
+        name={props.name}
+        rules={props.rules}
+        initialValue={props.initialValue}
+        noStyle
+      >
+        <Input
+          className={cls.textField__input}
+          placeholder={props.placeholder}
+          disabled={props.disabled}
+          onChange={props.onChange}
+          type={props.type}
+          maxLength={props.maxLength}
+        />
+      </FormItem>
+    </FormItem>
+  )
+}
