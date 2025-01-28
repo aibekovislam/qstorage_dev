aafee5b211825f495304f576e36086a5d3c617e0
Baimanchick <baikaimanchick@gmail.com>
2025-01-28 14:23:32 +0600

    fixed table structure

--- src/pages/inventory/pages/View.tsx
+++ src/pages/inventory/pages/View.tsx
@@ -2,7 +2,7 @@
 
 import React from 'react'
 
-import { Avatar, Popover, Space, Tag, Typography, Table, Flex } from 'antd'
+import { Avatar, Popover, Space, Typography, Table, Flex } from 'antd'
 import { ColumnsType } from 'antd/es/table'
 import Image from 'next/image'
 
@@ -36,37 +36,24 @@ const columns: ColumnsType<ProductRecord> = [
     ),
   },
   {
-    title: 'Проект',
-    dataIndex: 'project',
-    key: 'project',
+    title: 'Остаток',
+    dataIndex: 'stock',
+    key: 'stock',
   },
   {
-    title: 'Кол-во',
-    dataIndex: 'quantity',
-    key: 'quantity',
+    title: 'Закуп. цена (сом)',
+    dataIndex: 'purechase_price',
+    key: 'purechase_price',
   },
   {
-    title: 'Статус',
-    dataIndex: 'status',
-    key: 'status',
-    render: (status: string) => (
-      <Tag color="green">{status}</Tag>
-    ),
+    title: 'Сумма (закуп..сом)',
+    dataIndex: 'sum',
+    key: 'sum',
   },
   {
     title: 'Дата',
     dataIndex: 'date',
     key: 'date',
-  },
-  {
-    title: '№ Акта',
-    dataIndex: 'actNumber',
-    key: 'actNumber',
-  },
-  {
-    title: 'Поставщик',
-    dataIndex: 'supplier',
-    key: 'supplier',
   },
   {
     title: 'Ответственный',
@@ -97,12 +84,10 @@ const dataSource: ProductRecord[] = [
   {
     key: 1,
     product: 'Штаны',
-    project: 'TradeCode 99',
-    quantity: '3,638,066',
-    status: 'ПРОВЕРЕНО',
+    stock: 6462020,
+    purechase_price: 986,
     date: '2021-02-05',
-    actNumber: '574836839',
-    supplier: 'Vel crad sed rhoncus.',
+    sum: 757865,
     responsible: 'Чынгыз А.',
     comment: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Nisi cumque quibusdam officia, autem, laborum magnam consequatur, adipisci saepe incidunt sed sit magni repudiandae. Nesciunt fuga voluptate, quia quod voluptates cupiditate.',
     imageUrl: 'https://outdoorvitals.com/cdn/shop/products/greensatushopify.png?v=1701706579&width=1000',
--- src/pages/inventory/types.ts
+++ src/pages/inventory/types.ts
@@ -1,13 +1,11 @@
 export interface ProductRecord {
     key: React.Key
     product: string
-    project: string
-    quantity: string
-    status: string
     date: string
-    actNumber: string
-    supplier: string
     responsible: string
     comment: string
     imageUrl?: string
+    stock: number
+    purechase_price: number
+    sum: number
 }
